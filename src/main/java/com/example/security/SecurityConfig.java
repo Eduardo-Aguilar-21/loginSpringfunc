12daf3d7091df3b13616e35f4d741c09a165a234
Eduardo Aguilar <es123y123@gmail.com>
2023-06-29 19:26:44 -0500

    Login funcionando hasta fallo de estado

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import com.example.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,9 +18,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.HeaderWriter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import(WebConfig.class)
 public class SecurityConfig {
 
     @Autowired
@@ -31,41 +41,59 @@ public class SecurityConfig {
     JwtAuthorizationFilter authorizationFilter;
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        // Configurar los encabezados CORS adicionales
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
 
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils);
         jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
         jwtAuthenticationFilter.setFilterProcessesUrl("/login");
 
+        HeaderWriter headerWriter = new StaticHeadersWriter("Access-Control-Allow-Origin", "*");
+
         return httpSecurity
+                .cors()
+                .and()
                 .csrf(config -> config.disable())
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/hello").permitAll();
-                    auth.anyRequest().authenticated();
-                })
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .addFilter(jwtAuthenticationFilter)
                 .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers().addHeaderWriter(headerWriter)  // Agregar el encabezado Access-Control-Allow-Origin
+                .and()
+                .httpBasic().disable()  // Deshabilitar la autenticación básica
+                .formLogin().disable()  // Deshabilitar el formulario de inicio de sesión
+                .logout().disable()  // Deshabilitar la funcionalidad de logout
+                .exceptionHandling().disable()
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/hello").permitAll();
+                    auth.requestMatchers("/login").permitAll();
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
                 .build();
     }
-
-//    @Bean
-//    UserDetailsService userDetailsService(){
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("santiago")
-//                .password("1234")
-//                .roles()
-//                .build());
-//
-//        return manager;
-//    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-   }
+    }
 
     @Bean
     AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
